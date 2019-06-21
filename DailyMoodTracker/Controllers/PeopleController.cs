da2fe794eaefb64e92e52ffe53b7b53ff65fd886
Benjamin <btr21@pitt.edu>
2019-06-21 19:49:55 -0400

    Added all starter projects

---
+++ DailyMoodTracker/Controllers/PeopleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DailyMoodTracker.Models;
+
+namespace DailyMoodTracker.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Ben", LastName = "Rarrick", Age = 23, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Time", LastName = "dsa", Age = 23, IsAlive = true });
+            people.Add(new PersonModel { FirstName = "Bends", LastName = "Ragdrrick", Age = 23, IsAlive = true });
+
+            return View(people);
+        }
+    }
+}
